a9bd51b39a0ab98bb591a650af5575cd6e85e96d
Eslam Mohammed Anwar <eslam.anwar@almtar.com>
2019-10-04 14:29:25 +0200

    try new mongo config

--- config/mongo.js
+++ config/mongo.js
@@ -1,6 +1,6 @@
 module.exports = {
   development: {
-    url: 'mongodb://localhost:27017/contests'
+    url: 'mongodb://mongodb:27017/contests'
   },
   production: {
     url: 'mongodb://mongodb:27017/contests'
