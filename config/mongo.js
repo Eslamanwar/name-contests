d9f0a2c3dfc063c431b7811e5bc5ee4321dc080a
Eslam Mohammed Anwar <eslam.anwar@almtar.com>
2019-10-04 13:05:25 +0200

    push

--- config/mongo.js
+++ config/mongo.js
@@ -1,5 +1,8 @@
 module.exports = {
   development: {
-    url: 'mongodb://mongodb:27017/contests'
+    url: 'mongodb://localhost:27017/contests'
   }
+  production: {
+    url: 'mongodb://mongodb:27017/contests'
+  }	
 };
